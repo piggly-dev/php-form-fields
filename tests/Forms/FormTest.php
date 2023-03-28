b0d2740777e7f6bc8ab51d2937713d4ae41c70d2
Caique Araujo <caique@studiopiggly.com.br>
2023-03-28 19:48:24 -0300

    :sparkles: feat(fields): Fields Collection

--- tests/Forms/FormTest.php
+++ tests/Forms/FormTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Pgly\FormFields\Collections\FieldsCollection;
 use Pgly\FormFields\Fields\RenderAttributes\BasicRenderAttribute;
 use Pgly\FormFields\Fields\TextInputField;
 use Pgly\FormFields\Forms\Form;
@@ -11,61 +12,16 @@ use PHPUnit\Framework\TestCase;
 
 class FormTest extends TestCase
 {
-	public function testAddField()
-	{
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_name');
-		$field = new TextInputField($options);
-
-		$form = new Form();
-		$form->addField($field);
-		$this->assertContains($field, $form->getFields());
-	}
-
-	public function testGetField()
-	{
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_name');
-		$field = new TextInputField($options);
-
-		$form = new Form();
-		$form->addField($field);
-		$this->assertEquals($field, $form->getField('test_name'));
-	}
-
-	public function testRenderField()
-	{
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_name');
-		$field = new TextInputField($options);
-
-		$form = new Form();
-		$form->addField($field);
-		$this->assertIsString($form->renderField('test_field', new BasicRenderAttribute('')));
-	}
-
-	public function testRemoveField()
-	{
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_name');
-		$field = new TextInputField($options);
-
-		$form = new Form();
-		$form->addField($field);
-		$form->removeField('test_name');
-		$this->assertNotContains($field, $form->getFields());
-	}
-
 	public function testOptions()
 	{
 		$options = new HtmlFormOptions();
-		$form = new Form($options);
+		$form = new Form(new FieldsCollection(), $options);
 		$this->assertEquals($options, $form->options());
 	}
 
 	public function testToGroup()
 	{
-		$form = new Form();
+		$form = new Form(new FieldsCollection());
 		$group = $form->toGroup(new HtmlGroupFormOptions());
 		$this->assertInstanceOf(GroupForm::class, $group);
 	}
@@ -78,7 +34,7 @@ class FormTest extends TestCase
 		$options->changeMethod('POST');
 		$options->changeName('test_name');
 
-		$form = new Form($options);
+		$form = new Form(new FieldsCollection(), $options);
 
 		$expected_html  = '<form id="test_name" name="test_name" action="http://example.com" method="POST" >';
 		$expected_html .= '<div class="pgly-wps--row"><div class="pgly-wps--column">';
@@ -86,44 +42,6 @@ class FormTest extends TestCase
 		$expected_html .= '</div></div>';
 
 		$this->assertEquals($expected_html, $form->renderHeader());
-	}
-
-	public function testOrganizeFields()
-	{
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_one');
-		$field_1 = new TextInputField($options);
-
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_two')->changeColumnSize(6);
-		$field_2 = new TextInputField($options);
-
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_three')->changeColumnSize(6);
-		$field_3 = new TextInputField($options);
-
-		$options = new HtmlFieldOptions();
-		$options->changeName('test_four');
-		$field_4 = new TextInputField($options);
-
-		$form = new Form();
-		$form->addField($field_1);
-		$form->addField($field_2);
-		$form->addField($field_3);
-		$form->addField($field_4);
-
-		$this->assertEquals([
-			[
-				$field_1,
-			],
-			[
-				$field_2,
-				$field_3
-			],
-			[
-				$field_4
-			]
-		], $form->organizeFields());
 	}
 
 	public function testRenderFooter()
@@ -134,7 +52,7 @@ class FormTest extends TestCase
 		$options->method('POST');
 		$options->name('test_name');
 
-		$form = new Form($options);
+		$form = new Form(new FieldsCollection(), $options);
 
 		$expected_html  = '<div class="pgly-wps--row"><div class="pgly-wps--column">';
 		$expected_html .= '<button class="pgly-wps--button pgly-wps-is-primary pgly-async--behaviour pgly-form--submit">Submit<svg class="pgly-wps--spinner pgly-wps-is-white" viewBox="0 0 50 50"><circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle></svg></button>';
